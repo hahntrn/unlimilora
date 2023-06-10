61b209395ebf6e6650581e5eb78be70ab806909d
Justin Tinker <justin@Justins-MacBook-Air.local>
2023-06-09 23:19:47 -0700

    t

--- src/utils/custom_seq2seq_trainer.py
+++ src/utils/custom_seq2seq_trainer.py
@@ -123,13 +123,13 @@ class CustomTrainer(Seq2SeqTrainer):
          # Uri: to make sure we use cache even during mid-training evaluation, where this is disabled in general:
         gen_kwargs['use_cache'] = True
         #print(inputs)
-        for k in inputs:
-            print(k)
-            gen_kwargs[k] = inputs[k]
+        # for k in inputs:
+        #     print(k)
+        #     gen_kwargs[k] = inputs[k]
         #gen_kwargs[self.model.main_input_name] = generation_inputs
         #print(gen_kwargs)
         generated_tokens = self.model.generate(
-            #input_ids=generation_inputs,
+            generation_inputs,
             **gen_kwargs
         )
         # --------------------- addition compared to the source file --------------------
